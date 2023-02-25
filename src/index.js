928fd178e502b7c6f913729d344682c5221b1b22
Gavin <gavinporlich@gmail.com>
2023-02-24 18:50:04 -0600

    root loader made

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
+import { rootLoader } from "./routes/loaders/rootLoader";
 import "./index.css";
-
+import ErrorPage from "./error-page";
+import Contact from "./routes/contacts";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
